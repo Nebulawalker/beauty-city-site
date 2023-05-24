1e7f529f012ee49ae32e792b980e1c0bd696bd35
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-05-24 18:31:35 +0300

    Добавлена модель Service в админку

--- site_reception/admin.py
+++ site_reception/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, Service
 
 
 @admin.register(Client)
-class Client(admin.ModelAdmin):
+class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone_number')
+    search_fields = ('name', 'phone_number')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'duration',
+        'price',
+    )
+    search_fields = ('title', )
+    list_editable = ('duration', 'price')
