c0ce67a01526487b7bbb00789e285c6951266574
M1nZof <maxykl38@gmail.com>
2023-05-25 23:11:47 +0300

    Handling queries for page (linking DB with template)

--- site_reception/views.py
+++ site_reception/views.py
@@ -3,13 +3,21 @@ from .orm_commands import *
 
 
 def index(request):
+    saloons = []
     for saloon in get_saloons():
-        print(saloon.photo.path)
+        saloon_properties = {'title': saloon.title, 'address': saloon.address, 'photo': saloon.photo.url}
+        saloons.append(saloon_properties)
 
+    masters = []
     for master in get_masters():
-        print(master.photo.path)
+        master_properties = {'name': master.name, 'photo': master.photo.url}    # Докинуть отзывы и стаж работы
+        masters.append(master_properties)
 
+    services = []
     for service in get_services():
-        print(service.photo.path)
+        service_properties = {'title': service.title, 'price': service.price, 'photo': service.photo.url}
+        services.append(service_properties)
 
-    return render(request, 'index.html')
+    context = {'saloons': saloons, 'masters': masters, 'services': services}
+
+    return render(request, 'index.html', context=context)
