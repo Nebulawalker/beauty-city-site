0b76439aabede9aa9cd99f86e821c658448eddc4
SadRus <rystixx@yandex.ru>
2023-05-26 00:13:39 +0300

    Support for media urls

--- beauty_city_site/urls.py
+++ beauty_city_site/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from site_reception import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='Главная'),
     path('service.html', views.service, name='service'),
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
