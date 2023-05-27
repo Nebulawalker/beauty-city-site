169e7d9af4f03415e6363006b86db94d146aee67
M1nZof <maxykl38@gmail.com>
2023-05-27 12:47:42 +0300

    Added method to get reviews

--- site_reception/orm_commands.py
+++ site_reception/orm_commands.py
@@ -1,12 +1,17 @@
-from .models import *
+from .models import Master, Saloon, Service, Review
 
 
 def get_masters():
     return Master.objects.all()
 
+
 def get_saloons():
     return Saloon.objects.all()
+
 
 def get_services():
     return Service.objects.all()
 
+
+def get_reviews():
+    return Review.objects.select_related('client').all()
