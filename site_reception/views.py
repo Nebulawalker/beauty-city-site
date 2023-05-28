1e0536e90a969250bcee252dbc2359bd99e620f2
SadRus <rystixx@yandex.ru>
2023-05-28 22:48:15 +0300

    Add handle schedule

--- site_reception/views.py
+++ site_reception/views.py
@@ -5,6 +5,7 @@ from site_reception.models import Order
 from django.template.loader import render_to_string
 from datetime import datetime
 
+
 def index(request):
     saloons = []
     for saloon in get_saloons():
@@ -41,25 +42,12 @@ def service_finally(request):
 
 
 def service(request):
-    # service_title = request.POST.get('service')
-    # master_name = request.POST.get('master')
-
-    # orders = Order.objects.all()
-    # if service_title:
-    #     orders = orders.filter(service__title=service_title)
-    # if master_name:
-    #     orders = orders.filter(master__name=master_name)
-
-    # ordered_timeslots = []
-    # for order in orders:
-    #     ordered_timeslots.append(order.appointment_time)
-
     context = {
         'saloons': get_saloons(),
         'services': get_services(),
         'masters': get_masters(),
         'time_slots': get_timeslots(),
-        # 'ordered_timeslots': ordered_timeslots,
+        'ordered_timeslots': [],
     }
     return render(request, 'service.html', context)
 
@@ -80,4 +68,11 @@ def handle_schedule(request):
     for order in orders:
         if order.appointment_date == date:
             ordered_timeslots.append(order.appointment_time)
-    return JsonResponse({'ordered_timeslots': ordered_timeslots}, safe=False)
+    html = render_to_string(
+        'timeslots.html',
+        {
+            'ordered_timeslots': ordered_timeslots,
+            'time_slots': get_timeslots(),
+        }
+    )
+    return JsonResponse(html, safe=False)
